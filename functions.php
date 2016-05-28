b09040321cc022086af2b99e1f43859b6d58a992
Aditia A. Pratama <aditia.ap@gmail.com>
2016-05-28 14:10:34 +0700

    ada function to toggle map

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function divi_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_register_style( 'glyphicons', get_stylesheet_directory_uri() . '/css/glyphicons.css' );
     wp_enqueue_style('glyphicons');
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/scripts.js' );
 
 }
 
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Map overlay switch
+$('.map-toggle .switch').click(function () {
+    $(this).closest('.contact').toggleClass('toggle-active');
+    $(this).toggleClass('toggle-active');
+  }
+);
