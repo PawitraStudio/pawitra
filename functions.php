22436f29045abc5a821fc6caf174619027521de6
Aditia A. Pratama <aditia.ap@gmail.com>
2016-06-02 00:57:25 +0700

    adding intern-cta

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function divi_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_register_style( 'glyphicons', get_stylesheet_directory_uri() . '/css/glyphicons.css' );
     wp_enqueue_style('glyphicons');
-    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/scripts.js' );
+    wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery') );
 
 }
 ?>
