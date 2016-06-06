f2344ac543c572d713c9c75a0fb964cb9848c6bf
Aditia A. Pratama <aditia.ap@gmail.com>
2016-06-06 11:15:54 +0700

    add fontawesome

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'divi_enqueue_styles' );
 function divi_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_register_style( 'fontawesome', get_stylesheet_directory_uri() . '/css/fontawesome.min.css' );
+    wp_enqueue_style('fontawesome');
     wp_register_style( 'glyphicons', get_stylesheet_directory_uri() . '/css/glyphicons.css' );
     wp_enqueue_style('glyphicons');
     wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery') );
