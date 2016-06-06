1f07426406758a0436187837cd93124233a1acc8
Aditia A. Pratama <aditia.ap@gmail.com>
2016-06-06 11:52:21 +0700

    add font-mizz

--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 add_action( 'wp_enqueue_scripts', 'divi_enqueue_styles' );
 function divi_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_register_style( 'fontawesome', get_stylesheet_directory_uri() . '/css/fontawesome.min.css' );
+    wp_register_style( 'fontawesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css' );
     wp_enqueue_style('fontawesome');
+    wp_register_style( 'mfizz', get_stylesheet_directory_uri() . '/css/font-mfizz.css' );
+    wp_enqueue_style('mfizz');
     wp_register_style( 'glyphicons', get_stylesheet_directory_uri() . '/css/glyphicons.css' );
     wp_enqueue_style('glyphicons');
     wp_enqueue_script( 'script', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery') );
